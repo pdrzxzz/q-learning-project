c44a09140d65fba71f462ecc18eb05004260d5fa
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2025-07-03 22:11:30 -0300

    Train model a lot more

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,13 +1,12 @@
 from client import Platform, read_or_create_q_table, binary_to_position_id, select_action, update_q_value, save_q_table
 import connection
-import numpy as np
 
 # ================================
 # Q-Learning parameters
 # ================================
 LEARNING_RATE = 0.1       # Learning rate (alpha) -> MODEL CONVERGENCE # NÃO AUMENTAR
-DISCOUNT_FACTOR = 0.95     # Discount factor (gamma) -> RELATIVE TO PROBLEM
-EPSILON = 0               # Exploration rate (epsilon) -> TRAINING MUTÁVEL
+DISCOUNT_FACTOR = 0.8     # Discount factor (gamma) -> RELATIVE TO PROBLEM
+EPSILON = 0.1               # Exploration rate (epsilon) -> TRAINING MUTÁVEL
 
 # ================================
 # Action and Direction mappings
@@ -28,7 +27,7 @@ if socket == 0:
 q_table = read_or_create_q_table()
 
 # Number of training actions
-num_actions = 1000000
+num_actions = 10000
 
 # Initialize environment state
 state_str, reward = connection.get_state_reward(socket, "none")
@@ -39,7 +38,7 @@ total_reward = 0
 action_count = 0
 # Training loop
 for action in range(num_actions):
-    # LEARNING_RATE = max(0.1, LEARNING_RATE - 0.00005)
+    # LEARNING_RATE = max(0.1, LEARNING_RATE - (0.9 / (num_actions/2)))
     # EPSILON = LEARNING_RATE
     
     state_index, reward = current_state.get()
@@ -67,48 +66,13 @@ for action in range(num_actions):
         avg_reward = total_reward / action_count
         total_reward = 0
         action_count = 0
+        # print(f'Action: {action+1}')
         # print(f"Learning Rate: {LEARNING_RATE:.2f}")
         # print(f"Epsilon: {EPSILON:.2f}")
         print(f"Average Reward: {avg_reward:.2f}")
-        # Save Q-table to file
-    
+
+    # Save Q-table to file
     save_q_table(q_table, "data/new_q_table.txt")
     
     # Update internal state
     current_state.update(new_state_index, reward)
-
-# def find_optimal_value_to_gamma():
-#     num_actions = 1000
-#     for gamma in [0.8, 0.85, 0.9, 0.95, 0.99]:
-#         LEARNING_RATE = 1       # Learning rate (alpha)
-#         EPSILON = 1    
-#         q_table = np.zeros((24 * 4, 3), dtype=float)
-#         total_reward = 0
-#         for action in range(num_actions):
-#             LEARNING_RATE = max(0.1, LEARNING_RATE - 0.0005)
-#             EPSILON = LEARNING_RATE
-            
-#             state_index, reward = current_state.get()
-#             total_reward += reward
-            
-#             # Choose an action
-#             selected_action = select_action(q_table, state_index, EPSILON, ACTIONS)
-#             selected_action_index = ACTIONS.index(selected_action)
-
-#             # Execute action and get next state and reward
-#             new_state_str, reward = connection.get_state_reward(socket, selected_action)
-#             new_state_index = binary_to_position_id(new_state_str)
-
-#             # Update Q-table
-#             update_q_value(q_table, state_index, reward, new_state_index, selected_action_index, LEARNING_RATE, gamma)
-            
-#             avg_reward = total_reward / num_actions
-#             # print(f"Learning Rate: {LEARNING_RATE:.2f}")
-#             # print(f"Epsilon: {EPSILON:.2f}")
-            
-#             # Update internal state
-#             current_state.update(new_state_index, reward)
-
-#         print(f"Average Reward for gamma {gamma}: {avg_reward}")
-
-# find_optimal_value_to_gamma()
